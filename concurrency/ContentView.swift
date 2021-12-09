4c19f1b1c64b0a35e6b98d2da74000c1aa8646e3
Michael McGreal <mcgrealife@gmail.com>
2021-12-08 19:37:37 -0500

    Initial Commit

---
+++ concurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  concurrency
+//
+//  Created by Michael McGreal on 12/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ concurrency/concurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  concurrencyApp.swift
+//  concurrency
+//
+//  Created by Michael McGreal on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct concurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
